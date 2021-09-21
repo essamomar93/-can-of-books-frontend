f2576761b55653c7d8b159cc10454109de11b616
DimaAlabsi <dema.yaser8@gmail.com>
2021-09-21 20:39:22 +0300

    lab11

---
+++ src/components/BestBooks.js
@@ -0,0 +1,58 @@
+import React from 'react';
+// import axios from "axios";
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+class BestBooks extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     // books: []
+  //     // showData:false
+  //   }
+  // }
+
+  render() {
+    // console.log(this.state.books);
+
+    console.log(this.props.books)
+
+    return (
+      <>
+      {this.props.books.length >0 && (
+        <Carousel>
+          {
+            this.props.books.map((i) => {
+              return (
+               
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src="https://lh3.googleusercontent.com/bXB6ueK2wyb44f8A5Vxgf0_JmmTThXr7cqhUY9vr133RZkGguV2WGKV-Q4LTimmijCgO2zD3p3FxpfXcT3MALfLP3UQo8q2VpvzRLkj0Gg=s626"
+                      alt="First slide"
+                    />
+                    <Carousel.Caption>
+                      <h1>{i.title}</h1>
+                      <h3>{i.description}</h3>
+                      <h3>{i.status}</h3>
+                      <h3>{i.email}</h3>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+               
+              )
+            })
+          }
+        </Carousel>
+    
+      )}
+      
+      {this.props.books.length === 0 && (<h1>NO BOOKS FOUND</h1>
+        )}
+    </>
+  )
+}
+}
+
+export default BestBooks;
---
+++ src/components/BookForm.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+
+ class BookForm extends Component {
+    render() {
+        return (
+            <>
+               <form onSubmit={this.props.submitHandle}>
+          <input type="text" placeholder="Enter the book title" onChange={this.props.tiltleHandle}/>
+          <input type="text" placeholder="Description" onChange={this.props.descriptionHandle}/>
+          <input type="text" placeholder="status" onChange={this.props.statusHandle}/>
+          <input type="text" placeholder="Email" onChange={this.props.emailHandle}/>
+
+
+
+
+          <input type="submit" value="add a new BOOK "/>
+          <button onClick={()=>{this.props.handleDelete(this.props.id)}}>Delete</button>
+
+        </form>
+            </>
+        )
+    }
+}
+
+export default BookForm
